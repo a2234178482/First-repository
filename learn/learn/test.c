d24a9e2e079c1cfcd5318ca131f10be49309cf40
xiaoming-learns-java <2234178482@qq.com>
2024-01-19 22:16:04 +0800

    关于Switch和if

---
+++ learn/learn/test.c
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	sizeof(i++);
+//	printf("%d\n", i);
+//		
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i = 2;
+//	switch (i)
+//	{
+//	case 1:
+//		printf("i am cool!\n");
+//		break;
+//	case 2:
+//		printf("i am very cool!\n");
+//		break;
+//	case 3:
+//		printf("i am very very cool!\n");
+//		break;
+//	default:
+//		printf("i am smart!\n");
+//		break;
+//	}
+//}
+//
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//	switch (a)
+//	{
+//	case 1:
+//		printf("i am genius\n");
+//		break;
+//	case 2:
+//		printf("i know you\n");
+//		break;
+//	case 3:
+//		printf("i am fool\n");
+//		break;
+//	default:
+//		printf("where you are!\n");
+//		break;
+//
+//
+//		return 0;
+//	}
+//}
+int main()
+{
+	int  i = 1;
+	while (i <= 100)
+	{
+	
+		if (i < 50)
+			
+			printf("%d", i);
+		else
+			printf("jieshu");
+			i++;
+	}
+	
+
+
+
+
+	return 0;
+}
