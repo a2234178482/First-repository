e2baec028f31e568aa7c7877fd27d270344fe5ab
xiaoming-learns-java <2234178482@qq.com>
2024-01-22 22:20:32 +0800

    今日练习

---
+++ text_1_22/text_1_22/test.c
@@ -0,0 +1,126 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 10 == 9)
+//
+//			j++;
+//
+//		if (i / 10 == 9)
+//
+//			j++;
+//	}
+//	printf("%d", j);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a, b, c, t;
+//	(void)scanf("%d,%d,%d", &a, &b, &c);
+//	if(a > b) { t = a; a = b; b = t; }
+//	if(a > c) { t = a; a = c; c = t; }
+//	if(b > c) { t = b; b = c; c = t; }
+//	printf("%d %d %d", a, b, c);
+////}
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	int c = 3;
+//	(void)scanf("%d,%d,%d", &a, &b, &c);
+//	if ((a > b) && (a > c) && (b > c))
+//	{
+//		printf("%d %d %d", a , b , c);
+//	}
+//	else if ((a > b) && (a < c))
+//	{
+//		printf("%d %d %d", c, a, b);
+//	}
+//	else if ((a < b) && (a > c))
+//	{
+//		printf("%d %d %d", b, a, c);
+//	}
+//	else if ((a < b) && (a < c) && (b > c))
+//	{
+//		printf("%d %d %d", b, c, a);
+//	}
+//	else if ((a > b) && (a > c) && (b < c))
+//	{
+//		printf("%d %d %d", a, c, b);
+//	}
+//	else
+//	{
+//		printf("%d %d %d", c, b, a);
+//	}
+//	return  0;
+//}
+//int main()
+//{
+//    int a = 0, b = 0, c = 0, d = 0;
+//    scanf("%d %d", &a, &b);
+//    if (b == 0)
+//    {
+//        printf("shuruyouwu");
+//    }
+//    else
+//    {
+//        c = a / b;
+//        d = a % b;
+//        printf("%d %d", c, d);
+//    }
+//    return  0;
+//
+//
+//
+//}
+//int main()
+//{
+//    int a = 0, b = 0, c = 1;
+//    scanf("%d %d %d", &a, &b, &c);
+//    printf("%d", b);
+//    return 0;
+//}
+//int main()
+//{
+//	char a = 0;
+//	scanf("%c",&a);
+//	printf("%c%c%c", a, a, a);
+//	printf("\n");
+//	printf("%c%c%c", a, a, a);
+//	printf("\n");
+//	printf("%c%c%c", a, a, a);
+//	printf("\n");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char a = 0;
+//	scanf("%c", &a);
+//	printf("  %c  ", a);
+//	printf("\n");
+//	printf(" %c%c%c ", a, a, a);
+//	printf("\n");
+//	printf("%c%c%c%c%c", a, a, a, a, a);
+//	printf("\n");
+//	printf(" %c%c%c ", a, a, a);
+//	printf("\n");
+//	printf("  %c  ", a);
+//	return 0;
+//
+//}
+#include<stdio.h>
+int main()
+{
+    char i = 0;
+    scanf("%c", &i);
+    printf("%d\n", i);
+    return 0;
+}
+
