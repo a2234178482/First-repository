48a96332746c5b762e8b3d2ce7c2dbe9fa3454f3
xiaoming-learns-java <2234178482@qq.com>
2024-09-22 21:02:24 +0800

    链表

--- SLIST_9_21/Slist.c
+++ SLIST_9_21/Slist.c
@@ -89,3 +89,107 @@ void SLTPopBack(SLTNode** pphead)
 }
 //头删
 void SLTPopFront(SLTNode** pphead);
+{
+	//链表不能为空
+	assert(pphead &&  *pphead);
+	SLTNode* next = (*pphead)->next; //->优先级大于*
+	free(*pphead);
+	*phead = NULL;
+}
+
+//查找
+SLTNode* SLTFind(SLTNode* phead, SLTDataType x)
+{
+	SLTNode* pcur = phead;
+	while (pcur) //等价于pucr!= NULL
+	{
+		if (pcur->next == x)
+		{
+			return pcur;
+		}
+		pcur = pcur->next;
+	}
+	//pcur == NULL
+	return NULL;
+}
+
+//在指定位置之前插入数据
+void SLTInsert(SLTNode** pphead; SLTNode* pos; SLTDataType x)
+{
+	assert(pphead && *pphead);
+	assert(pos);
+	SLTNode* newnode = SLTBuyNode(x);
+	//若pos==*pphead ；说明是头插
+	if (pos == *pphead)
+	{
+		SLTPushFront(pphead, x);
+	}
+	else
+	{
+		SLTNode* prev = *pphead;
+		while (prev->next != pos)
+		{
+			prev = prev->next;
+		}
+		//prev->newnode->pos
+		newnode->next = pos;
+		prev->next = newnode;
+	}
+}
+
+//在指定位置之后插入数据
+void SLTInsertAfter(SLTNode* pos, SLTDataType x)
+{
+	assert(pos);
+	SLTNode* newnode = SLTBuyNode(x);
+	//pos->newnode->pos->next;
+	newnode->next = pos->next;
+	pos->next = newnode;
+}
+//删除pos节点
+void SLTErase(SLTNode** pphead, SLTNode* pos)
+{
+	assert(pphead && *pphead);
+	assert(pos);
+	//pos是头结点/pos不是头结点
+	if (pos == *pphead)
+	{
+		//头删
+		SLTPopFront(pphead);
+	}
+	else {
+		SLTNode* prev = *pphead;
+		while (prev->next != pos)
+		{
+			prev = prev->next;
+		}
+		//perv pos pos->next
+		prev->next = pos->next;
+		free(pos);
+		pos = NULL;
+	}
+}
+
+//删除pos之后的节点
+void SLTEraseAfter(SLTNode* pos)
+{
+	assert(pos && pos->next);
+	SLTNode* del = pos->next;
+	// pos del del->next
+	pos->next = del->next;
+	free(del);
+	del = NULL;
+}
+//销毁链表
+void SLTistDesTroy(SLTNode** pphead)
+{
+	assert(pphead && *pphead);
+	SLTNode* pcur = pcur->next;
+	while (pcur)
+	{
+		SLTNode* next = pcur->next;
+		free(pcur);
+		pcur = next;
+	}
+	*pphead = NULL;
+}
