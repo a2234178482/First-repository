171d88063aa01af21a49501e94497644d27bc892
xiaoming-learns-java <2234178482@qq.com>
2024-11-09 23:34:55 +0800

    二叉树

---
+++ test_11_9/test.c
@@ -0,0 +1,399 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"Heap.h"
+#include<time.h>
+
+void TestHeap1()
+{
+	int a[] = { 4,2,8,1,5,6,9,7,3,2,23,55,232,66,222,33,7,1,66,3333,999 };
+	HP hp;
+	HPInit(&hp);
+	for (size_t i = 0; i < sizeof(a) / sizeof(int); i++)
+	{
+		HPPush(&hp, a[i]);
+	}
+
+	int i = 0;
+	while (!HPEmpty(&hp))
+	{
+		printf("%d ", HPTop(&hp));
+		//a[i++] = HPTop(&hp);
+		HPPop(&hp);
+	}
+	printf("\n");
+
+	// 找出最大的前k个
+	/*int k = 0;
+	scanf("%d", &k);
+	while (k--)
+	{
+		printf("%d ", HPTop(&hp));
+		HPPop(&hp);
+	}
+	printf("\n");*/
+
+	HPDestroy(&hp);
+}
+
+// 堆排序    O(N*logN)
+// 冒泡排序  O(N^2) 
+void HeapSort(int* a, int n)
+{
+	// 降序，建小堆
+	// 升序，建大堆
+	// 向上调整建堆 O(N*logN)
+	/*for (int i = 1; i < n; i++)
+	{
+		AdjustUp(a, i);
+	}*/
+
+	// 向下调整建堆 O(N)
+	for (int i = (n - 1 - 1) / 2; i >= 0; i--)
+	{
+		AdjustDown(a, n, i);
+	}
+
+	// O(N*logN)
+	int end = n - 1;
+	while (end > 0)
+	{
+		Swap(&a[0], &a[end]);
+		AdjustDown(a, end, 0);
+		--end;
+	}
+}
+
+void TestHeap2()
+{
+	int a[] = { 4,2,8,1,5,6,9,7,2,7,9 };
+	HeapSort(a, sizeof(a) / sizeof(int));
+}
+
+void CreateNDate()
+{
+	// 造数据
+	int n = 100000;
+	srand(time(0));
+	const char* file = "data.txt";
+	FILE* fin = fopen(file, "w");
+	if (fin == NULL)
+	{
+		perror("fopen error");
+		return;
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		int x = (rand() + i) % 10000000;
+		fprintf(fin, "%d\n", x);
+	}
+
+	fclose(fin);
+}
+
+void TestHeap3()
+{
+	int k;
+	printf("请输入k>:");
+	scanf("%d", &k);
+	int* kminheap = (int*)malloc(sizeof(int) * k);
+	if (kminheap == NULL)
+	{
+		perror("malloc fail");
+		return;
+	}
+	const char* file = "data.txt";
+	FILE* fout = fopen(file, "r");
+	if (fout == NULL)
+	{
+		perror("fopen error");
+		return;
+	}
+
+	// 读取文件中前k个数
+	for (int i = 0; i < k; i++)
+	{
+		fscanf(fout, "%d", &kminheap[i]);
+	}
+
+	// 11:51继续
+	// 建K个数的小堆
+	for (int i = (k - 1 - 1) / 2; i >= 0; i--)
+	{
+		AdjustDown(kminheap, k, i);
+	}
+
+	// 读取剩下的N-K个数
+	int x = 0;
+	while (fscanf(fout, "%d", &x) > 0)
+	{
+		if (x > kminheap[0])
+		{
+			kminheap[0] = x;
+			AdjustDown(kminheap, k, 0);
+		}
+	}
+
+	printf("最大前%d个数：", k);
+	for (int i = 0; i < k; i++)
+	{
+		printf("%d ", kminheap[i]);
+	}
+	printf("\n");
+}
+
+//int main()
+//{
+//	//CreateNDate();
+//
+//	TestHeap3();
+//
+//	return 0;
+//}
+
+typedef int BTDataType;
+typedef struct BinaryTreeNode
+{
+	BTDataType data;
+	struct BinaryTreeNode* left;
+	struct BinaryTreeNode* right;
+}BTNode;
+
+BTNode* BuyNode(int x)
+{
+	BTNode* node = (BTNode*)malloc(sizeof(BTNode));
+	if (node == NULL)
+	{
+		perror("malloc fail");
+		return NULL;
+	}
+
+	node->data = x;
+	node->left = NULL;
+	node->right = NULL;
+
+	return node;
+}
+
+BTNode* CreatBinaryTree()
+{
+	BTNode* node1 = BuyNode(1);
+	BTNode* node2 = BuyNode(2);
+	BTNode* node3 = BuyNode(3);
+	BTNode* node4 = BuyNode(4);
+	BTNode* node5 = BuyNode(5);
+	BTNode* node6 = BuyNode(6);
+	BTNode* node7 = BuyNode(6);
+
+
+	node1->left = node2;
+	node1->right = node4;
+	node2->left = node3;
+	node4->left = node5;
+	node4->right = node6;
+	node5->right = node7;
+
+	return node1;
+}
+
+void PrevOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("N ");
+		return;
+	}
+
+	printf("%d ", root->data);
+	PrevOrder(root->left);
+	PrevOrder(root->right);
+}
+
+void InOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("N ");
+		return;
+	}
+
+	InOrder(root->left);
+	printf("%d ", root->data);
+	InOrder(root->right);
+}
+
+int fun(int n)
+{
+	if (n == 0)
+		return 0;
+
+	return fun(n - 1) + n;
+}
+
+// 错误示范
+//int TreeSize(BTNode* root)
+//{
+//	static int size = 0;
+//	if (root == NULL)
+//		return 0;
+//	else
+//		++size;
+//
+//	TreeSize(root->left);
+//	TreeSize(root->right);
+//
+//	return size;
+//}
+
+//int size = 0;
+//int TreeSize(BTNode* root)
+//{
+//	if (root == NULL)
+//		return 0;
+//	else
+//		++size;
+//
+//	TreeSize(root->left);
+//	TreeSize(root->right);
+//
+//	return size;
+//}
+
+//void TreeSize(BTNode* root, int* psize)
+//{
+//	if (root == NULL)
+//		return 0;
+//	else
+//		++(*psize);
+//
+//	TreeSize(root->left, psize);
+//	TreeSize(root->right, psize);
+//}
+
+int TreeSize(BTNode* root)
+{
+	return root == NULL ? 0 :
+		TreeSize(root->left) + TreeSize(root->right) + 1;
+}
+
+int TreeLeafSize(BTNode* root)
+{
+	if (root == NULL)
+		return 0;
+
+	if (root->left == NULL && root->right == NULL)
+		return 1;
+
+	return TreeLeafSize(root->left)
+		+ TreeLeafSize(root->right);
+}
+
+int TreeHeight(BTNode* root)
+{
+	if (root == NULL)
+		return 0;
+
+	int leftHeight = TreeHeight(root->left);
+	int rightHeight = TreeHeight(root->right);
+
+	return leftHeight > rightHeight ?
+		leftHeight + 1 : rightHeight + 1;
+}
+
+// 有效率问题
+//int TreeHeight(BTNode* root)
+//{
+//	if (root == NULL)
+//		return 0;
+//
+//	return TreeHeight(root->left) > TreeHeight(root->right) ?
+//		TreeHeight(root->left) + 1 : TreeHeight(root->right) + 1;
+//}
+
+//int fmax(int x, int y)
+//{
+//	return x > y ? x : y;
+//}
+
+//int TreeHeight(BTNode* root)
+//{
+//	if (root == NULL)
+//		return 0;
+//
+//	return fmax(TreeHeight(root->left), TreeHeight(root->right)) + 1;
+//}
+// 二叉树第k层节点个数
+int TreeLevelKSize(BTNode* root, int k)
+{
+	if (root == NULL)
+		return 0;
+
+	if (k == 1)
+		return 1;
+
+	// 子问题
+	return TreeLevelKSize(root->left, k - 1)
+		+ TreeLevelKSize(root->right, k - 1);
+}
+
+// 二叉树查找值为x的节点
+BTNode* TreeFind(BTNode* root, BTDataType x)
+{
+	if (root == NULL)
+		return NULL;
+
+	if (root->data == x)
+		return root;
+
+	BTNode* ret1 = TreeFind(root->left, x);
+	if (ret1)
+		return ret1;
+
+	BTNode* ret2 = TreeFind(root->right, x);
+	if (ret2)
+		return ret2;
+
+	return NULL;
+}
+
+//BTNode* TreeFind(BTNode* root, BTDataType x)
+//{
+//	if (root == NULL)
+//		return NULL;
+//
+//	if (root->data == x)
+//		return root;
+//
+//	BTNode* ret1 = TreeFind(root->left, x);
+//	if (ret1)
+//		return ret1;
+//
+//	return TreeFind(root->right, x);
+//}
+
+int main()
+{
+	BTNode* root = CreatBinaryTree();
+	PrevOrder(root);
+	printf("\n");
+
+	InOrder(root);
+	printf("\n");
+
+	//printf("%d\n", fun(10000));
+
+	/*int size = 0;
+	TreeSize(root, &size);
+	printf("TreeSize:%d\n",size);
+
+	size = 0;
+	TreeSize(root, &size);
+	printf("TreeSize:%d\n", size);*/
+
+	printf("TreeSize:%d\n", TreeSize(root));
+	printf("TreeLeafSize:%d\n", TreeLeafSize(root));
+	printf("TreeHeight:%d\n", TreeHeight(root));
+	printf("TreeLevelKSize:%d\n", TreeLevelKSize(root, 3));
+	printf("TreeLevelKSize:%d\n", TreeLevelKSize(root, 4));
+
+	return 0;
+}
