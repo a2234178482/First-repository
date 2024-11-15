472655f37c88e53b97fbd40f8399c362adf9e532
xiaoming-learns-java <2234178482@qq.com>
2024-11-15 23:23:00 +0800

    快排

---
+++ test_11_15/Sort.c
@@ -0,0 +1,320 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"Sort.h"
+
+void PrintArray(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}
+
+void Swap(int* p1, int* p2)
+{
+	int tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+// 时间复杂度：O(N^2)  什么情况最坏：逆序
+// 最好：顺序有序，O(N)
+// 插入排序
+void InsertSort(int* a, int n)
+{
+	//  [0, n-1]
+	for (int i = 0; i < n - 1; i++)
+	{
+		// [0, n-2]是最后一组
+		// [0,end]有序 end+1位置的值插入[0,end]，保持有序
+		int end = i;
+		int tmp = a[end + 1];
+		while (end >= 0)
+		{
+			if (tmp < a[end])
+			{
+				a[end + 1] = a[end];
+				--end;
+			}
+			else
+			{
+				break;
+			}
+		}
+		a[end + 1] = tmp;
+	}
+}
+
+// O(N^1.3)
+//void ShellSort(int* a, int n)
+//{
+//	/*int gap = 3;
+//	for (int j = 0; j < gap; j++)
+//	{
+//		for (size_t i = j; i < n - gap; i += gap)
+//		{
+//			int end = i;
+//			int tmp = a[end + gap];
+//			while (end >= 0)
+//			{
+//				if (tmp < a[end])
+//				{
+//					a[end + gap] = a[end];
+//					end -= gap;
+//				}
+//				else
+//				{
+//					break;
+//				}
+//			}
+//			a[end + gap] = tmp;
+//		}
+//	}*/
+//
+//	int gap = n;
+//	while (gap > 1)
+//	{
+//		// +1保证最后一个gap一定是1
+//		// gap > 1时是预排序
+//		// gap == 1时是插入排序
+//		gap = gap / 3 + 1;
+//
+//		for (size_t i = 0; i < n - gap; ++i)
+//		{
+//			int end = i;
+//			int tmp = a[end + gap];
+//			while (end >= 0)
+//			{
+//				if (tmp < a[end])
+//				{
+//					a[end + gap] = a[end];
+//					end -= gap;
+//				}
+//				else
+//				{
+//					break;
+//				}
+//			}
+//			a[end + gap] = tmp;
+//		}
+//		//printf("gap:%2d->", gap);
+//		//PrintArray(a, n);
+//	}
+//}
+
+// O(N ^ 1.3)
+void ShellSort(int* a, int n)
+{
+	int gap = n;
+	while (gap > 1)
+	{
+		// +1保证最后一个gap一定是1
+		// gap > 1时是预排序
+		// gap == 1时是插入排序
+		gap = gap / 3 + 1;
+
+		for (size_t i = 0; i < n - gap; ++i)
+		{
+			int end = i;
+			int tmp = a[end + gap];
+			while (end >= 0)
+			{
+				if (tmp < a[end])
+				{
+					a[end + gap] = a[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a[end + gap] = tmp;
+		}
+	}
+}
+
+
+void AdjustDown(int* a, int n, int parent)
+{
+	// 先假设左孩子小
+	int child = parent * 2 + 1;
+
+	while (child < n)  // child >= n说明孩子不存在，调整到叶子了
+	{
+		// 找出小的那个孩子
+		if (child + 1 < n && a[child + 1] > a[child])
+		{
+			++child;
+		}
+
+		if (a[child] > a[parent])
+		{
+			Swap(&a[child], &a[parent]);
+			parent = child;
+			child = parent * 2 + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void HeapSort(int* a, int n)
+{
+	// 向下调整建堆 O(N)
+	for (int i = (n - 1 - 1) / 2; i >= 0; i--)
+	{
+		AdjustDown(a, n, i);
+	}
+
+	// O(N*logN)
+	int end = n - 1;
+	while (end > 0)
+	{
+		Swap(&a[0], &a[end]);
+		AdjustDown(a, end, 0);
+		--end;
+	}
+}
+
+// O(N^2) 最坏
+// O(N)   最好
+void BubbleSort(int* a, int n)
+{
+	for (int j = 0; j < n; j++)
+	{
+		// 单趟
+		int flag = 0;
+		for (int i = 1; i < n - j; i++)
+		{
+			if (a[i - 1] > a[i])
+			{
+				Swap(&a[i - 1], &a[i]);
+				flag = 1;
+			}
+		}
+
+		if (flag == 0)
+		{
+			break;
+		}
+	}
+}
+
+// O(N^2)
+void SelectSort(int* a, int n)
+{
+	int begin = 0, end = n - 1;
+
+	while (begin < end)
+	{
+		int mini = begin, maxi = begin;
+		for (int i = begin + 1; i <= end; ++i)
+		{
+			if (a[i] > a[maxi])
+			{
+				maxi = i;
+			}
+
+			if (a[i] < a[mini])
+			{
+				mini = i;
+			}
+		}
+
+		Swap(&a[begin], &a[mini]);
+		if (begin == maxi)
+			maxi = mini;
+
+		Swap(&a[end], &a[maxi]);
+		++begin;
+		--end;
+	}
+}
+
+int GetMidi(int* a, int left, int right)
+{
+	int midi = (left + right) / 2;
+	// left midi right
+	if (a[left] < a[midi])
+	{
+		if (a[midi] < a[right])
+		{
+			return midi;
+		}
+		else if (a[left] < a[right])
+		{
+			return right;
+		}
+		else
+		{
+			return left;
+		}
+	}
+	else // a[left] > a[midi]
+	{
+		if (a[midi] > a[right])
+		{
+			return midi;
+		}
+		else if (a[left] < a[right])
+		{
+			return left;
+		}
+		else
+		{
+			return right;
+		}
+	}
+}
+
+// 避免有序情况下，效率退化
+// 1、随机选key
+// 2、三数取中
+// 21：14
+// 面试手撕，不用三数取中和小区间优化
+// 后续讲一下思路即可
+void QuickSort(int* a, int left, int right)
+{
+	if (left >= right)
+		return;
+
+	// 小区间优化，不再递归分割排序，减少递归的次数
+	if ((right - left + 1) < 10)
+	{
+		InsertSort(a + left, right - left + 1);
+	}
+	else
+	{
+		// 三数取中
+		int midi = GetMidi(a, left, right);
+		Swap(&a[left], &a[midi]);
+
+		int keyi = left;
+		int begin = left, end = right;
+		while (begin < end)
+		{
+			// 右边找小
+			while (begin < end && a[end] >= a[keyi])
+			{
+				--end;
+			}
+
+			// 左边找大
+			while (begin < end && a[begin] <= a[keyi])
+			{
+				++begin;
+			}
+
+			Swap(&a[begin], &a[end]);
+		}
+
+		Swap(&a[keyi], &a[begin]);
+		keyi = begin;
+		// [left, keyi-1] keyi [keyi+1, right]
+		QuickSort(a, left, keyi - 1);
+		QuickSort(a, keyi + 1, right);
+	}
+}
---
+++ test_11_15/test.c
@@ -0,0 +1,216 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXQSIZE 10
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+
+typedef char QElemType;
+typedef int Status;
+
+/***队列的顺序存储结构***/
+typedef struct {
+	QElemType* base;//初始化时动态分配存储空间
+	int front;//头指针
+	int rear;//尾指针
+} SqQueue;
+
+/***循环队列基本操作***/
+
+//算法3.11　循环队列的初始化
+Status InitQueue(SqQueue* Q) {//构造一个空队列Q
+	Q->base = (QElemType*)malloc(MAXQSIZE * sizeof(QElemType));//为队列分配一个最大容量为MAXSIZE的数组空间
+	if (!Q->base)
+		exit(OVERFLOW); //存储分配失败
+	Q->front = Q->rear = 0; //头指针和尾指针置为零，队列为空
+	return OK;
+}
+
+//算法3.12　求循环队列的长度
+int QueueLength(SqQueue Q) {//返回Q的元素个数，即队列的长度
+	return (Q.rear - Q.front + MAXQSIZE) % MAXQSIZE;
+}
+
+//算法3.13　循环队列的入队
+Status EnQueue(SqQueue* Q, QElemType e) {//插入元素e为Q的新的队尾元素
+	if ((Q->rear + 1) % MAXQSIZE == Q->front) //尾指针在循环意义上加1后等于头指针，表明队满
+		return ERROR;
+	Q->base[Q->rear] = e; //新元素插入队尾
+	Q->rear = (Q->rear + 1) % MAXQSIZE; //队尾指针加1
+	return OK;
+}
+
+//算法3.14　循环队列的出队
+Status DeQueue(SqQueue* Q, QElemType* e) {//删除Q的队头元素，用e返回其值
+	if (Q->front == Q->rear)
+		return ERROR; //队空
+	*e = Q->base[Q->front]; //保存队头元素
+	Q->front = (Q->front + 1) % MAXQSIZE; //队头指针加1
+	return OK;
+}
+
+//算法3.15　取循环队列的队头元素
+QElemType GetHead(SqQueue Q) {//返回Q的队头元素，不修改队头指针
+	if (Q.front != Q.rear) //队列非空
+		return Q.base[Q.front]; //返回队头元素的值，队头指针不变
+}
+
+//释放内存 
+void DestroySqQueue(SqQueue* Q) {
+	free(Q->base);
+}
+
+int main() {
+	SqQueue Q;
+	InitQueue(&Q);
+
+	char input[MAXQSIZE];
+	fgets(input, sizeof(input), stdin);
+	input[strcspn(input, "\n")] = '\0';
+
+	for (int i = 0; input[i] != '\0'; i++) {
+		if (input[i] != ' ') {
+			if (EnQueue(&Q, input[i]) == ERROR) {
+				printf("队列已满，无法继续输入。\n");
+				break;
+			}
+		}
+	}
+	while (Q.front != Q.rear) {
+		QElemType e;
+		DeQueue(&Q, &e);
+		printf("%c ", e);
+	}
+	printf("\n");
+
+	DestroySqQueue(&Q);
+	return 0;
+}
+#include"Sort.h"
+
+void TestInsertSort()
+{
+	int a[] = { 2,4,1,7,8,3,9,2 };
+	InsertSort(a, sizeof(a) / sizeof(int));
+	PrintArray(a, sizeof(a) / sizeof(int));
+}
+
+void TestShellSort()
+{
+	int a[] = { 9,1,2,5,7,4,6,3,5,9,1,2,5,7,4,6,3,5,9,1,2,5,7,4,6,3,5,9,1,2,5,7,4,6,3,5,9,1,2,5,7,4,6,3,5 };
+	//InsertSort(a, sizeof(a) / sizeof(int));
+
+	PrintArray(a, sizeof(a) / sizeof(int));
+
+	ShellSort(a, sizeof(a) / sizeof(int));
+	PrintArray(a, sizeof(a) / sizeof(int));
+}
+
+void TestSelectSort()
+{
+	//int a[] = { 9,1,2,5,7,4,6,3,5,9,1,2,5,7,4,6,3,5,9,1,2,5,7,4,6,3,5,9,1,2,5,7,4,6,3,5,9,1,2,5,7,4,6,3,5 };
+	//InsertSort(a, sizeof(a) / sizeof(int));
+	//int a[] = { 2,4,1,7,8,3,9,2 };
+	int a[] = { 9,1,2,5,7,4,6,3 };
+
+	PrintArray(a, sizeof(a) / sizeof(int));
+	SelectSort(a, sizeof(a) / sizeof(int));
+	PrintArray(a, sizeof(a) / sizeof(int));
+}
+
+void TestQuickSort()
+{
+	int a[] = { 6,1,2,7,9,3,4,5,10,8 };
+
+	PrintArray(a, sizeof(a) / sizeof(int));
+	QuickSort(a, 0, sizeof(a) / sizeof(int) - 1);
+	PrintArray(a, sizeof(a) / sizeof(int));
+}
+
+void TestOP()
+{
+	srand(time(0));
+	const int N = 100000;
+	int* a1 = (int*)malloc(sizeof(int) * N);
+	int* a2 = (int*)malloc(sizeof(int) * N);
+	int* a3 = (int*)malloc(sizeof(int) * N);
+	int* a4 = (int*)malloc(sizeof(int) * N);
+	int* a5 = (int*)malloc(sizeof(int) * N);
+	int* a6 = (int*)malloc(sizeof(int) * N);
+	int* a7 = (int*)malloc(sizeof(int) * N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		// ÖØ¸´²»¶à
+		a1[i] = rand() + i;
+		// ÖØ¸´½Ï¶à
+		//a1[i] = rand();
+		a2[i] = a1[i];
+		a3[i] = a1[i];
+		a4[i] = a1[i];
+		a5[i] = a1[i];
+		a6[i] = a1[i];
+		a7[i] = a1[i];
+	}
+
+	int begin1 = clock();
+	//InsertSort(a1, N);
+	int end1 = clock();
+
+	int begin2 = clock();
+	ShellSort(a2, N);
+	int end2 = clock();
+
+	PrintArray(a2, N);
+
+	int begin3 = clock();
+	//SelectSort(a3, N);
+	int end3 = clock();
+
+	int begin4 = clock();
+	HeapSort(a4, N);
+	int end4 = clock();
+
+	int begin5 = clock();
+	QuickSort(a5, 0, N - 1);
+	int end5 = clock();
+
+	int begin6 = clock();
+	//MergeSort(a6, N);
+	int end6 = clock();
+
+	int begin7 = clock();
+	//BubbleSort(a7, N);
+	int end7 = clock();
+
+	printf("InsertSort:%d\n", end1 - begin1);
+	printf("ShellSort:%d\n", end2 - begin2);
+	printf("SelectSort:%d\n", end3 - begin3);
+	printf("HeapSort:%d\n", end4 - begin4);
+	printf("QuickSort:%d\n", end5 - begin5);
+	printf("MergeSort:%d\n", end6 - begin6);
+	printf("BubbleSort:%d\n", end7 - begin7);
+
+	free(a1);
+	free(a2);
+	free(a3);
+	free(a4);
+	free(a5);
+	free(a6);
+	free(a7);
+}
+
+
+int main()
+{
+	//TestInsertSort();
+	//TestShellSort();
+	//TestSelectSort();
+	//TestQuickSort();
+
+	TestOP();
+
+	return 0;
+}
