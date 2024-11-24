fa3ee57a304c2e8c56bb53ef023dd75c00f8db94
xiaoming-learns-java <2234178482@qq.com>
2024-11-24 23:57:29 +0800

    补充

--- test_11_24/test.c
+++ test_11_24/test.c
@@ -39,3 +39,129 @@
 //    }
 //    return NULL;
 //}
+void STInit(ST* pst) {
+	assert(pst);
+	pst->a = NULL;
+	//top指向栈顶数据的下一个
+	pst->top = 0;
+	pst->capacity = 0;
+}
+
+void STDestroy(ST* pst) {
+	assert(pst);
+	free(pst->a);
+	pst->a = NULL;
+	pst->top = pst->capacity = 0;
+}
+
+void STPush(ST* pst, STDataType x) {
+	assert(pst);
+	if (pst->top == pst->capacity) {
+		int newcapacity = pst->capacity == 0 ? 4 : pst->capacity * 2;
+		STDataType* tmp = (STDataType*)realloc(pst->a, newcapacity * sizeof(STDataType));
+		if (tmp == NULL) {
+			perror("realloc fail");
+			return;
+		}
+		pst->a = tmp;
+		pst->capacity = newcapacity;
+	}
+	pst->a[pst->top] = x;
+	pst->top++;
+}
+void STPop(ST* pst) {
+	assert(pst);
+	assert(pst->top > 0);
+	pst->top--;
+}
+
+bool  STEmpty(ST* pst) {
+	assert(pst);
+	return pst->top == 0;
+}
+
+int STSize(ST* pst) {
+	assert(pst);
+	return pst->top;
+}
+
+typedef int STDataType;
+typedef struct Stack {
+	STDataType* a;
+	int top;
+	int capacity;
+}ST;
+void STInit(ST* pst) {
+	assert(pst);
+	pst->a = NULL;
+	//top指向栈顶数据的下一个
+	pst->top = 0;
+	pst->capacity = 0;
+}
+
+void STDestroy(ST* pst) {
+	assert(pst);
+	free(pst->a);
+	pst->a = NULL;
+	pst->top = pst->capacity = 0;
+}
+
+void STPush(ST* pst, STDataType x) {
+	assert(pst);
+	if (pst->top == pst->capacity) {
+		int newcapacity = pst->capacity == 0 ? 4 : pst->capacity * 2;
+		STDataType* tmp = (STDataType*)realloc(pst->a, newcapacity * sizeof(STDataType));
+		if (tmp == NULL) {
+			perror("realloc fail");
+			return;
+		}
+		pst->a = tmp;
+		pst->capacity = newcapacity;
+	}
+	pst->a[pst->top] = x;
+	pst->top++;
+}
+void STPop(ST* pst) {
+	assert(pst);
+	assert(pst->top > 0);
+	pst->top--;
+}
+STDataType STTop(ST* pst)
+{
+	assert(pst);
+	assert(pst->top > 0);
+
+	return pst->a[pst->top - 1];
+}
+bool  STEmpty(ST* pst) {
+	assert(pst);
+	return pst->top == 0;
+}
+
+int STSize(ST* pst) {
+	assert(pst);
+	return pst->top;
+}
+bool isValid(char* s) {
+	ST st;
+	STInit(&st);
+	while (*s) {
+		if (*s == '(' || *s == '[' || *s == '{') {
+			STPush(&st, *s);
+		}
+		else {
+			if (STEmpty(&st)) {
+				return false;
+			}
+			char top = STTop(&st);
+			STPop(&st);
+			if (top == '(' && *s != ')' ||
+				top == '[' && *s != ']' ||
+				top == '{' && *s != '}')
+				return false;
+		}
+		++s;
+	}
+	int ret = STEmpty(&st);
+	return ret;
+}
