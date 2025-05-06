d209f3b628b6cc3f4432d5aae99d7186c429c7ed
xiaoming-learns-java <2234178482@qq.com>
2025-05-06 22:23:46 +0800

    string的实现

---
+++ test_2025_4_29/string.h
@@ -0,0 +1,105 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <assert.h>
+using namespace std;
+namespace niuma
+{
+	class string {
+	public:
+		typedef char* iterator;
+		typedef const char* const_iterator;
+		iterator begin() {
+			return _str;
+		}
+		iterator end()
+		{
+			return _str + _size;
+		}
+		const_iterator begin() const
+		{
+			return _str;
+		}
+		const_iterator end() const
+		{
+			return _str + _size;
+		}
+		string(const char* str = "")
+		{
+			_size = strlen(str);
+			_capacity = _size;
+			_str = new char[_capacity + 1];
+			strcpy(_str, str);
+		}
+		string(const string& s)
+		{
+			if (this != &s)
+			{
+				delete[] _str;
+				_str = new char[_capacity + 1];
+				strcpy(_str, s._str);
+				_size = s._size;
+				_capacity = s._capacity;
+			}
+		}
+		const char* c_str() const
+		{
+			return _str;
+		}
+		void clear()
+		{
+			_str[0] = '\0';
+			_size = 0;
+		}
+		~string()
+		{
+			delete[] _str;
+			_str = nullptr;
+			_size = _capacity = 0;
+		}
+		size_t size() const
+		{
+			return _size;
+		}
+		size_t capacity() const
+		{
+			return _capacity;
+		}
+		char& operator[](size_t pos)
+		{
+			assert(pos < _size);
+			return _str[pos];
+		}
+		const char& operator[](size_t pos)const
+		{
+			assert(pos < _size);
+			return _str[pos];
+		}
+		void reserve(size_t n);
+		void push_back(char ch);
+		void append(const char* str);
+		string& operator+=(char ch);
+		string& operator+=(const char* str);
+
+		void insert(size_t pos, char ch);
+		void insert(size_t pos, const char* str);
+		void erase(size_t pos, size_t len = npos);
+		size_t find(char ch, size_t pos = 0);
+		size_t find(const char* str, size_t pos = 0);
+		string substr(size_t pos = 0, size_t len = npos);
+	private:
+		char* _str;
+		size_t _size;
+		size_t _capacity;
+		static const size_t npos;
+	};
+	bool operator<(const string& s1, const string& s2);
+	bool operator<=(const string& s1, const string& s2); 
+	bool operator>(const string& s1, const string& s2);
+	bool operator>=(const string& s1, const string& s2);
+	bool operator==(const string& s1, const string& s2);
+	bool operator!=(const string& s1, const string& s2);
+	
+	ostream& operator<<(ostream& out, const string& s);
+	istream& operator>>(istream& in, const string& s);
+}
---
+++ test_2025_4_29/test.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <string>
+#include <map>
+#include <list>
+#include "string.h"
+using namespace std;
+int main() {
+	string str;
+	while (getline(cin, str)) {
+		int pos = str.rfind(' ');
+		cout << str.size() - pos - 1 << endl;
+	}
+	char16_t str16[] = u"niuma";
+	char32_t str32[] = U"niuma";
+	wchar_t wstr[] = L"niuma";
+}
